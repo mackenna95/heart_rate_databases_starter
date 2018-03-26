a70c1c7944ab3dc05633c2ac88ebfc6bab314b83
mackenna95 <hillmj@umich.edu>
2018-03-26 13:53:29 -0400

    pep8

--- main.py
+++ main.py
@@ -3,30 +3,38 @@ import models
 import datetime
 connect("mongodb://localhost:27017/heart_rate_app")  # open up connection to db
 
+
 def get_info(email, heart_rate, time):
     """
     Gets heart_rate measurements from the user specified by email.
     :param email: str email of the user
     """
-    user = models.User.objects.raw({"_id": email}).first()  # Get the first user where _id=email
+    # Get the first user where _id=email
+    user = models.User.objects.raw({"_id": email}).first()
+
 
 def add_heart_rate(email, heart_rate, time):
     """
-    Appends a heart_rate measurement at a specified time to the user specified by
-    email. It is assumed that the user specified by email exists already.
+    Appends a heart_rate measurement at a specified time to the user specified
+    by email. It is assumed that the user specified by email exists already.
     :param email: str email of the user
     :param heart_rate: number heart_rate measurement of the user
     :param time: the datetime of the heart_rate measurement
     """
-    user = models.User.objects.raw({"_id": email}).first()  # Get the first user where _id=email
-    user.heart_rate.append(heart_rate)  # Append the heart_rate to the user's list of heart rates
-    user.heart_rate_times.append(time)  # append the current time to the user's list of heart rate times
+    # Get the first user where _id=email
+    user = models.User.objects.raw({"_id": email}).first()
+    # Append the heart_rate to the user's list of heart rates
+    user.heart_rate.append(heart_rate)
+    # append the current time to the user's list of heart rate times
+    user.heart_rate_times.append(time)
     user.save()  # save the user to the database
+
 
 def create_user(email, age, heart_rate, time):
     """
-    Creates a user with the specified email and age. If the user already exists in the DB this WILL
-    overwrite that user. It also adds the specified heart_rate to the user
+    Creates a user with the specified email and age. If the user already
+    exists in the DB this WILL overwrite that user. It also adds the
+    specified heart_rate to the user
     :param email: str email of the new user
     :param age: number age of the new user
     :param heart_rate: number initial heart_rate of this new user
@@ -37,18 +45,22 @@ def create_user(email, age, heart_rate, time):
     u.heart_rate_times.append(time)  # add initial heart rate time
     u.save()  # save the user to the database
 
+
 def print_user(email):
     """
     Prints the user with the specified email
     :param email: str email of the user of interest
     :return:
     """
-    user = models.User.objects.raw({"_id": email}).first()  # Get the first user where _id=email
+    # Get the first user where _id=email
+    user = models.User.objects.raw({"_id": email}).first()
     print(user.email)
     print(user.heart_rate)
     print(user.heart_rate_times)
 
 if __name__ == "__main__":
-    create_user(email="suyash@suyashkumar.com", age=24, heart_rate=60, time=datetime.datetime.now())  # we should only do this once, otherwise will overwrite existing user
+    # we should only do this once, otherwise will overwrite existing user
+    create_user(email="suyash@suyashkumar.com", age=24, heart_rate=60,
+                time=datetime.datetime.now())
     add_heart_rate("suyash@suyashkumar.com", 60, datetime.datetime.now())
     print_user("suyash@suyashkumar.com")
