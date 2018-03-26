7f31f59379ddccb766672d97abdf5d89e8fd8821
mackenna95 <hillmj@umich.edu>
2018-03-26 18:10:54 -0400

    heart_rate function adds patient data into system and adds patient to database if not present

--- server.py
+++ server.py
@@ -1,16 +1,73 @@
 from flask import Flask, jsonify, request
 import math
+from main import get_info, add_heart_rate, create_user, print_user
+import datetime
 app = Flask(__name__)
 
 
 @app.route("/api/heart_rate", methods=["POST"])
-def sum():
+def heart_rate():
     # """
     # Returns sum of a and b to the caller
     # """
+
     r = request.get_json()  # parses the POST request body as JSON
-    s = r["a"][0] + r["b"][0]  # adds JSON dict parameter "a" and "b" together
-    return jsonify(s), 200
+
+    import logging
+    logging.basicConfig(filename="server_log.txt",
+                        format='%(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    try:
+        if not isinstance(r['user_email'], str):
+            raise TypeError("TypeError: user_email is not a sting")
+            return 400
+    except KeyError:
+        logging.debug('KeyError: incorrect user_email key')
+        raise KeyError("KeyError: incorrect user_email key")
+        return 400
+    try:
+        if not isinstance(r['user_age'], int):
+            raise TypeError("TypeError: user_age is not a sting")
+            return 400
+    except KeyError:
+        logging.debug('KeyError: incorrect user_age key')
+        raise KeyError("KeyError: incorrect user_age key")
+        return 400
+    try:
+        if isinstance(r['heart_rate'], int):
+            a=1
+        elif isinstance(r['heart_rate'], float):
+            a=1
+        else:
+            raise TypeError("TypeError: heart_rate is not a sting")
+            return 400
+    except KeyError:
+        logging.debug('KeyError: incorrect heart_rate key')
+        raise KeyError("KeyError: incorrect heart_rate key")
+        return 400
+
+    try:
+        add_heart_rate(r['user_email'], r['heart_rate'], datetime.datetime.now())
+    except Exception:
+        create_user(r['user_email'], r['user_age'], r['heart_rate'], datetime.datetime.now())
+
+    message = {
+        "message": "Post Completed",
+    }
+    return jsonify(message), 200
+    
+@app.route("/api/heart_rate2", methods=["POST"])
+def heart_rate2():
+    # """
+    # Returns sum of a and b to the caller
+    # """
+
+    r = request.get_json()  # parses the POST request body as JSON
+    message = {
+        "message": r['user_email'],
+    }
+    return jsonify(message), 200
 
 
 @app.route("/api/heart_rate/<user_email>", methods=["GET"])
@@ -20,8 +77,6 @@ def allHeartRate(user_email):
     """
     message = []
     message.append("Hello there ")
-    message.append(name)
-    message = ''.join(message)
     hello_name = {
         "message": message,
     }
